ed69c37f0a9d59d50f8d4dcccf926e65713d793d
Duc Nguyen <ducnguyen0305@gmail.com>
2022-03-14 13:37:49 -0700

    Add timed triggered Popup.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import Popup from "./components/Popup";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [buttonPopup, setButtonPopup] = useState(false);
+  const [timedPopup, setTimedPopup] = useState(false);
+
+  // -- Use useEffect to call this only one time
+  useEffect(() => {
+    // -- Use setTimeout() to delay the execution
+    setTimeout(() => {
+      setTimedPopup(true);
+    }, 3000);
+  }, []);
 
   return (
     <div className="App">
@@ -13,6 +22,12 @@ function App() {
 
       {/* -- This Popup is trigged by clicking on Open Popup button */}
       <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>My popup</h3>
+        <p>This is my button triggerd popup</p>
+      </Popup>
+
+      {/* -- This Popup is triggered by timed */}
+      <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
         <h3>My popup</h3>
         <p>This is my button triggerd popup</p>
       </Popup>
