666d678789cd0bbde4546310109d37807a7ceb65
Duc Nguyen <ducnguyen0305@gmail.com>
2022-03-14 13:27:06 -0700

    Trigger Popup by clicking.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Popup from "./components/Popup";
+import { useState } from "react";
+
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false);
+
   return (
     <div className="App">
-     React App with POPUP Compomnent
+      <main>
+        <h1>React Popup</h1>
+        <button onClick={() => setButtonPopup(true)}>Open Popup</button>
+      </main>
+
+      {/* -- This Popup is trigged by clicking on Open Popup button */}
+      <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>My popup</h3>
+        <p>This is my button triggerd popup</p>
+      </Popup>
     </div>
   );
 }
